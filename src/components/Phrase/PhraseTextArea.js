cd3b7111585d45d65794beb88bf8609e4f3e2e49
student <noeline.mar@onja.org>
2021-04-22 15:31:17 +0300

    Handle the input in the story and wrap the element

--- src/components/Phrase/PhraseTextArea.js
+++ src/components/Phrase/PhraseTextArea.js
@@ -1,45 +1,48 @@
-// components/Task.js
 import * as React from 'react';
-import { Text, SafeAreaView, TextInput, StyleSheet } from 'react-native';
-// import { styles } from '../constants/globalStyles';
+import { SafeAreaView, TextInput, StyleSheet } from 'react-native';
 
-export default function PhraseTextArea({ label, text, placeholderText, isEditable, ...restProps }) {
-  const [ inputValue, setInputValue ] = React.useState(text)
-  const [ isFocus, setIsFocus ] = React.useState(false)
 
-    const styles = StyleSheet.create({
-      container: {
-        marginLeft: 23,
-        marginRight: 23,
-      },
-      input: {
-        borderColor: '#E5E5E5',
-        backgroundColor: '#fff',
-        borderStyle: 'solid',
-        borderWidth: 1,
-        borderRadius: 3,
-        width: '100%',
-        textAlign: 'center',
-        height: 100,
-        color: '#111827',
-        padding: 23,
-        fontSize: 20,
-        lineHeight: 24,
-        fontWeight: 'normal',
-      },
-    })
+const styles = StyleSheet.create({
+  input: {
+    borderColor: '#E5E5E5',
+    backgroundColor: '#fff',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderRadius: 3,
+    width: '100%',
+    textAlign: 'center',
+    height: 100,
+    color: '#111827',
+    paddingLeft: 56,
+    paddingRight: 56,
+    paddingTop: 20,
+    paddingBottom: 21,
+    fontSize: 20,
+    lineHeight: 24,
+    fontWeight: 'normal',
+  },
+})
+
+export default function PhraseTextArea({ 
+  placeholderText, 
+  placeholderTextColor, 
+  isEditable, 
+  value, 
+  onchange 
+}) {
 
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView>
       <TextInput 
         style={styles.input} 
         multiline={true}
+        numberOfLines={3}
+
         placeholder={placeholderText}
-        placeholderTextColor={'rgba(17, 24, 39, 0.5)'}
-        value={inputValue}
-        onChangeText={(text) => setInputValue(text)}
+        placeholderTextColor={placeholderTextColor}
         editable={isEditable}
-        {...restProps}
+        value={value}
+        onChangeText={onchange}
       />
     </SafeAreaView>
   );
--- src/components/Phrase/PhraseTextArea.stories.js
+++ src/components/Phrase/PhraseTextArea.stories.js
@@ -8,6 +8,35 @@ import PhraseTextArea from "./PhraseTextArea"
 
 
 storiesOf('PhraseTextArea', module)
-  .addDecorator(story => <View>{story()}</View>)
-  .add('Uneditable', () => <PhraseTextArea placeholderText= {'Enter Text'} text={'rua ambin’ny folo'} isEditable= {false} />)
-  .add('Editable', () => <PhraseTextArea placeholderText= {'Enter Text'} />)
+  .addDecorator(story => <View style={{margin: 23}}>{story()}</View>)
+  .add('Uneditable', () => (
+    <View>
+      <PhraseTextArea 
+        value={'You have answered all the questions in this category'} 
+        isEditable= {false} 
+      />
+      <PhraseTextArea 
+        value={'roa ambin’ny folo'} 
+        isEditable= {false} 
+      />
+    </View>
+  ))
+  .add('Editable', () => {
+    function Parent({ children }) {
+      const [ inputValue, setInputValue ] = React.useState('')
+      return <View>{children(inputValue, setInputValue)}</View>
+    }
+
+    return (
+      <Parent>
+        {(inputValue, setInputValue) => (
+          <PhraseTextArea 
+            value={inputValue}
+            placeholderText={'Enter here'}
+            onchange={(text) => setInputValue(text)}
+            placeholderTextColor={'rgba(17, 24, 39, 0.5)'}
+          />
+        )}
+      </Parent>
+    )
+  })
