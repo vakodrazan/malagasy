51f280a33319a00112b5c563dba40599425c4dcb
student <noeline.mar@onja.org>
2021-04-24 14:17:47 +0300

    Styled the element

--- src/components/SectionHeading/SectionHeading.js
+++ src/components/SectionHeading/SectionHeading.js
@@ -1,11 +1,24 @@
 import * as React from 'react';
-import { Text, SafeAreaView } from 'react-native';
+import { Text, SafeAreaView, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'flex-start',
+        alignItems: 'center'
+    },
+    headingText: {
+        fontWeight: '600',
+        fontSize: 18,
+        lineHeight: 22,
+        color: '#111827'
+    }
+})
 
 export default function SectionHeading({ title }) {
 
   return (
     <SafeAreaView >
-      <Text>{title}</Text>
+      <Text style={styles.headingText}>{title}</Text>
     </SafeAreaView>
   );
 }
