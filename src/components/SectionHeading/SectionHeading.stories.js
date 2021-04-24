592ff51cc82eb7de8891a771e8d841830d564be0
student <noeline.mar@onja.org>
2021-04-24 14:25:24 +0300

    Make two story for the long and short text

--- src/components/SectionHeading/SectionHeading.stories.js
+++ src/components/SectionHeading/SectionHeading.stories.js
@@ -1,11 +1,20 @@
 
 import * as React from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 
 import { storiesOf } from '@storybook/react-native';
 import SectionHeading from './SectionHeading';
 
+const styles = StyleSheet.create({
+    wrapper: {
+        paddingTop: 30,
+        paddingBottom: 15,
+        paddingLeft: 23,
+        paddingLeft: 23,
+    }
+})
 
 storiesOf('SectionHeading', module)
-  .addDecorator(story => <View>{story()}</View>)
-  .add('Heading', () => <SectionHeading title= {'Heading'}/>)
+  .addDecorator(story => <View style={styles.wrapper}>{story()}</View>)
+  .add('Heading short', () => <SectionHeading title= {'Category:'} />)
+  .add('Heading long', () => <SectionHeading title= {'The phrase in Malagasy:'}/>)
