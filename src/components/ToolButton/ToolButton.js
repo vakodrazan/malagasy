fff0166d2d391db5cdc2a7c85fc4e37a34d3dfa9
student <noeline.mar@onja.org>
2021-04-26 16:45:26 +0300

    Create a button component and test it in the stories with different icon

--- src/components/ToolButton/ToolButton.js
+++ src/components/ToolButton/ToolButton.js
@@ -1,10 +1,29 @@
 import * as React from 'react';
-import { Text, SafeAreaView } from 'react-native';
+import {Text, SafeAreaView, TouchableOpacity, StyleSheet} from 'react-native';
+import {Icon} from 'react-native-elements';
 
-export default function ToolButton({text}) {
-    return (
-        <SafeAreaView>
-            <Text>{text}</Text>
-        </SafeAreaView>
-    )
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonContainer: {
+    width: 40,
+    height: 40,
+    backgroundColor: '#06B6D4',
+    textAlign: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 100,
+  },
+});
+
+export default function ToolButton({name, type, size}) {
+  return (
+    <SafeAreaView style={styles.container}>
+      <TouchableOpacity style={styles.buttonContainer}>
+        <Icon name={name} type={type} color="#fff" size={size} />
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
 }
--- src/components/ToolButton/ToolButton.stories.js
+++ src/components/ToolButton/ToolButton.stories.js
@@ -1,10 +1,27 @@
 import * as React from 'react';
-import { View } from 'react-native';
+import {View} from 'react-native';
 
-import { storiesOf } from '@storybook/react-native';
+import {storiesOf} from '@storybook/react-native';
 import ToolButton from './ToolButton';
-
 
 storiesOf('ToolButton', module)
   .addDecorator(story => <View>{story()}</View>)
-  .add('button', () => <ToolButton text= {'Button'}/>)
+  .add('Add', () => (
+    <ToolButton name={'plus'} type={'material-community'} size={14} />
+  ))
+  .add('Seen', () => (
+    <ToolButton name={'check'} type={'material-community'} size={17.6} />
+  ))
+  .add('Learnt', () => (
+    <ToolButton name={'check-all'} type={'material-community'} size={23.25} />
+  ))
+  .add('Back to previous', () => (
+    <ToolButton name={'chevron-left'} type={'material-community'} />
+  ))
+  .add('Switch Mode', () => (
+    <ToolButton
+      name={'brightness-6'}
+      type={'material-community'}
+      size={22.62}
+    />
+  ));
