6ae3ee24c85031bf09280131a639923036d125bd
student <noeline.mar@onja.org>
2021-04-27 15:19:09 +0300

    Add some style for the text and test it with different text

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -6,11 +6,22 @@ import ActionButton from '../ActionButton/ActionButton';
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#d3d3d3',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderColor: '#E5E5E5',
   },
   listContent: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
+  },
+  itemTitle: {
+    color: '#111827',
+    fontSize: 16,
+    lineHeight: 19,
+    fontWeight: '400',
+    paddingRight: 11,
+    flex: 1,
   },
 });
 
@@ -24,10 +35,15 @@ export default function ListItemFunction({
   size,
 }) {
   return (
-    <TouchableOpacity style={styles.container}>
+    <TouchableOpacity style={styles.container} onPress={onPress}>
       <ListItem>
         <ListItem.Content style={styles.listContent}>
-          <ListItem.Title>{title}</ListItem.Title>
+          <ListItem.Title
+            style={styles.itemTitle}
+            numberOfLines={1}
+            ellipsizeMode="tail">
+            {title}
+          </ListItem.Title>
           <ActionButton
             text={text}
             name={buttonName}
--- src/components/ListItem/ListItem.stories.js
+++ src/components/ListItem/ListItem.stories.js
@@ -6,9 +6,31 @@ import ListItemFunction from './ListItem';
 
 storiesOf('ListIten', module)
   .addDecorator(story => <View style={{padding: 20}}>{story()}</View>)
-  .add('List item', () => (
+  .add('List item learn', () => (
     <ListItemFunction
-      title={'All'}
+      title={'All '}
+      text={'Learn'}
+      buttonName={'arrow-right'}
+      type={'material-community'}
+      color={'#06B6D4'}
+      onPress={action('learn-button')}
+      size={16}
+    />
+  ))
+  .add('List item pick', () => (
+    <ListItemFunction
+      title={'All '}
+      text={'Pick'}
+      buttonName={'arrow-right'}
+      type={'material-community'}
+      color={'#06B6D4'}
+      onPress={action('learn-button')}
+      size={16}
+    />
+  ))
+  .add('List item and long', () => (
+    <ListItemFunction
+      title={'An unncessessarly loooong category text is here '}
       text={'Learn'}
       buttonName={'arrow-right'}
       type={'material-community'}
