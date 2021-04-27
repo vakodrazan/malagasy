25f9c4c84d9b177e8f4b46e6cacc5db724185f0f
student <noeline.mar@onja.org>
2021-04-27 15:15:04 +0300

    Add some style for the item also wrap it inside of a touchable to make it clickable

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -1,16 +1,43 @@
 import * as React from 'react';
-import {SafeAreaView, Text} from 'react-native';
-import {ListItem, Avatar} from 'react-native-elements';
+import {StyleSheet, TouchableOpacity} from 'react-native';
+import {ListItem} from 'react-native-elements';
+import ActionButton from '../ActionButton/ActionButton';
 
-export default function ListItemFunction({key, title}) {
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#d3d3d3',
+  },
+  listContent: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+});
+
+export default function ListItemFunction({
+  title,
+  text,
+  buttonName,
+  type,
+  color,
+  onPress,
+  size,
+}) {
   return (
-    <SafeAreaView>
-      <Text>{title}</Text>
-      <ListItem key={key}>
-        <ListItem.Content>
+    <TouchableOpacity style={styles.container}>
+      <ListItem>
+        <ListItem.Content style={styles.listContent}>
           <ListItem.Title>{title}</ListItem.Title>
+          <ActionButton
+            text={text}
+            name={buttonName}
+            type={type}
+            color={color}
+            onPress={onPress}
+            size={size}
+          />
         </ListItem.Content>
       </ListItem>
-    </SafeAreaView>
+    </TouchableOpacity>
   );
 }
--- src/components/ListItem/ListItem.stories.js
+++ src/components/ListItem/ListItem.stories.js
@@ -1,8 +1,19 @@
 import * as React from 'react';
 import {View} from 'react-native';
-
 import {storiesOf} from '@storybook/react-native';
+import {action} from '@storybook/addon-actions';
 import ListItemFunction from './ListItem';
+
 storiesOf('ListIten', module)
   .addDecorator(story => <View style={{padding: 20}}>{story()}</View>)
-  .add('List item', () => <ListItemFunction key={'all'} title={'All'} />);
+  .add('List item', () => (
+    <ListItemFunction
+      title={'All'}
+      text={'Learn'}
+      buttonName={'arrow-right'}
+      type={'material-community'}
+      color={'#06B6D4'}
+      onPress={action('learn-button')}
+      size={16}
+    />
+  ));
