ad1359e4f3bea0d574338ca29c89ced9c86cad1c
student <noeline.mar@onja.org>
2021-04-27 15:47:22 +0300

    Using different action button

--- src/components/ListItem/ListItem.stories.js
+++ src/components/ListItem/ListItem.stories.js
@@ -24,7 +24,7 @@ storiesOf('ListIten', module)
       buttonName={'arrow-right'}
       type={'material-community'}
       color={'#06B6D4'}
-      onPress={action('learn-button')}
+      onPress={action('picked-button')}
       size={16}
     />
   ))
@@ -37,5 +37,27 @@ storiesOf('ListIten', module)
       color={'#06B6D4'}
       onPress={action('learn-button')}
       size={16}
+    />
+  ))
+  .add('List item and correct', () => (
+    <ListItemFunction
+      title={'two-thousand '}
+      text={'Correct'}
+      buttonName={'check'}
+      type={'material-community'}
+      color={'#06D440'}
+      onPress={action('checked-button')}
+      size={17.6}
+    />
+  ))
+  .add('List item and wrong', () => (
+    <ListItemFunction
+      title={'1494 '}
+      text={'Wrong'}
+      buttonName={'close'}
+      type={'material-community'}
+      color={'#D4068E'}
+      onPress={action('wrong-button')}
+      size={14}
     />
   ));
