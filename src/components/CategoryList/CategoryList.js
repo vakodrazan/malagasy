335f76832834f0559591175db60bcb06319e43d2
student <noeline.mar@onja.org>
2021-04-29 10:53:34 +0300

    Show the list of categories

--- src/components/CategoryList/CategoryList.js
+++ src/components/CategoryList/CategoryList.js
@@ -1,13 +1,30 @@
 import * as React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
 
-import categories from '../../data/categories.json';
+import categoryList from '../../data/categories.json';
+import List from '../List/List';
 
 export default function CategoryList() {
-  console.log(categories);
+  const {categories} = useSelector(state => state);
+  const dispatch = useDispatch();
+  React.useEffect(() => {
+    dispatch({type: 'CATEGORY_LIST', value: categoryList.categories});
+  }, []);
+
   return (
     <SafeAreaView>
-      <Text>Category List</Text>
+      <List
+        data={categories}
+        label={'Select a category: '}
+        text={'Learn'}
+        buttonName={'arrow-right'}
+        type={'material-community'}
+        color={'#06B6D4'}
+        onPress={alert('learn-button')}
+        size={16}
+        language={'en'}
+      />
     </SafeAreaView>
   );
 }
