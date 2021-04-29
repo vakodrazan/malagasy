a9c3886c304e79851c0aa61b3d800392c070cc8b
student <noeline.mar@onja.org>
2021-04-29 11:18:33 +0300

    Show the home screen

--- src/components/CategoryList/CategoryList.js
+++ src/components/CategoryList/CategoryList.js
@@ -1,9 +1,23 @@
 import * as React from 'react';
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, StyleSheet, View} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
 
 import categoryList from '../../data/categories.json';
 import List from '../List/List';
+import ToolButton from '../ToolButton/ToolButton';
+import LanguageSwitcherButton from '../LanguageSwitcherButton/LanguageSwitcherButton';
+
+const styles = StyleSheet.create({
+  container: {
+    marginRight: 23,
+    marginLeft: 23,
+  },
+  toolBar: {
+    flexDirection: 'row',
+    paddingBottom: 56,
+    paddingTop: 35,
+  },
+});
 
 export default function CategoryList() {
   const {categories} = useSelector(state => state);
@@ -13,7 +27,40 @@ export default function CategoryList() {
   }, []);
 
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.toolBar}>
+        <ToolButton
+          onPress={alert('Add-button')}
+          name={'plus'}
+          type={'material-community'}
+          size={14}
+        />
+        <LanguageSwitcherButton
+          primaryText={'EN'}
+          secondaryText={'MG'}
+          name="swap-horizontal"
+          type="material-community"
+          onPress={alert('Switch-language')}
+        />
+        <ToolButton
+          onPress={alert('Seen-button')}
+          name={'check'}
+          type={'material-community'}
+          size={17.6}
+        />
+        <ToolButton
+          onPress={alert('Learnt-button')}
+          name={'check-all'}
+          type={'material-community'}
+          size={23.25}
+        />
+        <ToolButton
+          onPress={alert('switch-mode-button')}
+          name={'brightness-6'}
+          type={'material-community'}
+          size={22.62}
+        />
+      </View>
       <List
         data={categories}
         label={'Select a category: '}
