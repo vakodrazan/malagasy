9223ec26d0d31ddbcaaf5d285a68dd0a6281547c
Noeline <noeline.mar@onja.org>
2021-05-07 17:55:58 +0300

    Since there is to need for the style in the app remove it

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import type {Node} from 'react';
-import {StyleSheet} from 'react-native';
 
 import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
@@ -27,24 +26,5 @@ const App: () => Node = () => {
     </Provider>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
