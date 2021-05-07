393dbe11753416f709056734f38bb031c9c9d5a6
Noeline <noeline.mar@onja.org>
2021-05-08 00:25:40 +0300

    Change the primary and secondary language after clicking

--- src/components/ToolButton/ToolButton.js
+++ src/components/ToolButton/ToolButton.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {Text, SafeAreaView, TouchableOpacity, StyleSheet} from 'react-native';
+import {SafeAreaView, TouchableOpacity, StyleSheet} from 'react-native';
 import {Icon} from 'react-native-elements';
 
 const styles = StyleSheet.create({
@@ -19,10 +19,18 @@ const styles = StyleSheet.create({
   },
 });
 
-export const switchLanguage = language => {
-  const dispatch = useDispatch();
-  dispatch({type: 'SHOW_NEXT_BUTTON', payload: 'mg'});
-};
+export function switchLanguage(dispatch, language) {
+  if (language === 'en') {
+    dispatch({type: 'SWITCH_LANGUAGE', payload: 'mg'});
+    dispatch({type: 'UDDATE_PRIMARY_LANGUAGE', payload: 'MG'});
+    dispatch({type: 'UDDATE_SECONDARY_LANGUAGE', payload: 'EN'});
+  } else {
+    dispatch({type: 'SWITCH_LANGUAGE', payload: 'en'});
+    dispatch({type: 'UDDATE_SECONDARY_LANGUAGE', payload: 'MG'});
+    dispatch({type: 'UDDATE_PRIMARY_LANGUAGE', payload: 'EN'});
+    dispatch({type: 'UDDATE_SECONDARY_LANGUAGE', payload: 'MG'});
+  }
+}
 
 export default function ToolButton({name, type, size, onPress}) {
   return (
