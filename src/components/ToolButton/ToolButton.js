b806c655c0cba6248105a5f2de4f49845dac22ca
Noeline <noeline.mar@onja.org>
2021-05-08 03:18:17 +0300

    Move the function into its own folder

--- src/components/ToolButton/ToolButton.js
+++ src/components/ToolButton/ToolButton.js
@@ -19,19 +19,6 @@ const styles = StyleSheet.create({
   },
 });
 
-export function switchLanguage(dispatch, language) {
-  if (language === 'en') {
-    dispatch({type: 'SWITCH_LANGUAGE', payload: 'mg'});
-    dispatch({type: 'UDDATE_PRIMARY_LANGUAGE', payload: 'MG'});
-    dispatch({type: 'UDDATE_SECONDARY_LANGUAGE', payload: 'EN'});
-  } else {
-    dispatch({type: 'SWITCH_LANGUAGE', payload: 'en'});
-    dispatch({type: 'UDDATE_SECONDARY_LANGUAGE', payload: 'MG'});
-    dispatch({type: 'UDDATE_PRIMARY_LANGUAGE', payload: 'EN'});
-    dispatch({type: 'UDDATE_SECONDARY_LANGUAGE', payload: 'MG'});
-  }
-}
-
 export default function ToolButton({name, type, size, onPress}) {
   return (
     <SafeAreaView style={styles.container}>
